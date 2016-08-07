76e6004569aed9f9fa77cf2259a1bd18b2a99698
rafadut <rafaeldsoliveira@outlook.com>
2016-08-07 20:02:49 -0300

    writing the test first

--- ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -31,14 +32,19 @@ namespace ForgetTheMilk.Controllers
         {
 
             Description = task ;
-            var dueDatePattern = new Regex(@"august\s(\d)");
+            var dueDatePattern = new Regex(@"(
+                |jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec|
+            )\s(\d)");
+
             var hasDueDate = dueDatePattern.IsMatch(task);
 
             if (hasDueDate)
             {
                 var dueDate = dueDatePattern.Match(task);
-                var day = Convert.ToInt32(dueDate.Groups[1].Value);
-                DueDate = new DateTime(DateTime.Today.Year, 8, day);
+                var monthInput = dueDate.Groups[1].Value;
+                var month = DateTime.ParseExact(monthInput, "MMM", CultureInfo.GetCultureInfo("en-us")).Month;
+                var day = Convert.ToInt32(dueDate.Groups[2].Value);
+                DueDate = new DateTime(DateTime.Today.Year, month, day);
                 if (DueDate < today)
                 {
                     DueDate = DueDate.Value.AddYears(1);
